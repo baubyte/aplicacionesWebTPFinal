c3ed73969afaaf5766eac6a09db4657957101387
Ariel <caminosariel@gmail.com>
2020-11-21 23:06:23 -0300

    Agregado al index

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -23,15 +23,15 @@
                         <div class="carousel-item active">
                         <img src="<?php echo URL_ROOT ?>/img/TorneoAjedrez.png" class="d-block w-100" alt="TorneoAjedrez" height="50%" width="50%">
                         <div class="carousel-caption d-none d-md-block">
-                            <h5>Convocatoria Toreno de Ajedréz</h5>
-                            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                            <h5></h5>
+                            <p>Convocatoria torneo de ajedrez</p>
                         </div>
                         </div>
                         <div class="carousel-item">
                         <img src="<?php echo URL_ROOT ?>/img/EstudiantesBarbijos.jpg" class="d-block w-100" alt="EstudiantesBarbijos" height="50%" width="50%">
                         <div class="carousel-caption d-none d-md-block">
                             <h5>La nueva normalidad en UNLZ</h5>
-                            <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                            <p>Presentamos los protocolos que estarán vigentes en la vuelta a las aulas</p>
                         </div>
                         </div>
                     </div>
@@ -44,24 +44,23 @@
                         <span class="sr-only">Next</span>
                     </a>
                     </div>
+            </div><br>
+            <div class="col-6 col-md-4">
+                <h2>Últimas noticias</h2><br>
+                <div class="twitter">
+                    <a class="twitter-timeline" data-width="400" data-height="400" data-dnt="true" data-theme="light" href="https://twitter.com/UNLZoficial?ref_src=twsrc%5Etfw"></a>
+                    <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+                </div>
             </div>
-        <div class="col-6 col-md-4">
-            <h2>Últimas noticias</h2><br>
-            <div class="twitter">
-                <a class="twitter-timeline" data-width="400" data-height="400" data-dnt="true" data-theme="light" href="https://twitter.com/UNLZoficial?ref_src=twsrc%5Etfw"></a>
-                <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
-            </div>
+    </div><br><br>
+    <div class="card text-center">
+        <div class="card-header">
+            <p style="font-weight: 900; color:#0c71b9">Escaneá y visitá nuestra página web para estar siempre informado.</p> 
         </div>
-    </div><br>
-    <div class="row justify-content-center">
-        <div class="col-6 col-md-4">
-        <u>¡Visitá nuestra página escaneando el código QR!</u><img class="imgQR" src='https://chart.googleapis.com/chart?cht=qr&chl=https%3A%2F%2Fwww.unlz.edu.ar%2F&chs=180x180&choe=UTF-8&chld=L|2' alt='qr code' width="100px" height="100px">
-        <a href='https://www.qr-code-generator.com' border='0' style='cursor:default'  rel='nofollow'></a>
-        </div>
+    <div class="card-body">
+        <p class="card-text"><img src="<?php echo URL_ROOT ?>/img/QR.png" alt="QRwebpage" srcset="" width="150px" height="150px"></p>
     </div>
-    <div class="container">
-
-    </div>
+</div>
 </article>
 <!-- Alta de Usuarios -->
 <?php require APP_ROOT . "/views/partial/footer.php"; ?>
