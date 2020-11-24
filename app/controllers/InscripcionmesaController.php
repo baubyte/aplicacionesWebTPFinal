ccee487bddc7806d941a866b05c17ea5a99f69d2
BAUBYTE <paredbaez.martin@gmail.com>
2020-11-24 12:37:33 -0300

    Ver Inscripciones a mesas de un usuario

--- app/controllers/InscripcionmesaController.php
+++ app/controllers/InscripcionmesaController.php
@@ -33,6 +33,23 @@ class InscripcionmesaController extends Controller
       ];
       return $this->view('inscripcionmesa/index', $data);
    }
+       /**
+     * Método Por el cual el usuario ve todas sus inscripciones
+     * Muestra la vista con el Listado de las Inscripciones
+     * disponibles.
+     * 
+     * @return void
+     */
+   public function show()
+   {
+      /**Comprobamos que sea un Alumno */
+      isAlumno();
+      $data = [
+         'titulo' => 'Inscripciones Mesas Finales',
+         'dataTables' => 'InscripcionMesasUsuario',
+      ];
+      return $this->view('inscripcionmesa/show', $data);
+   }
    /**
     * Se obtiene todos las Mesas de Finales Habilitadas
     * En Formato JSON para ser Usado en DataTables
@@ -79,7 +96,52 @@ class InscripcionmesaController extends Controller
       /**Mostramos el resultado en JSON */
       echo json_encode($response);
    }
-
+ /**
+    * Se obtiene todos las inscripciones de las Mesas de un usuario
+    * En Formato JSON para ser Usado en DataTables
+    *
+    * @return json
+    */
+    public function getInscripcionMesasHasUserDt()
+    {
+       /**Array para el response */
+       $dataResponse = [];
+       /**Recibimos lo Valores de DataTable */
+       $columnIndex = $_GET['order'][0]['column']; // Índice de columna
+       $draw = $_GET['draw'];
+       $data = [
+          'row' => $_GET['start'], //Desde que registro para registro por pagina
+          'rowPerPage' => $_GET['length'], //Hasta que registro para Registros por Pagina
+          'columnName' => $_GET['columns'][$columnIndex]['data'], // Nombre de la Columna
+          'columnSortOrder' => $_GET['order'][0]['dir'], // Orden ASC o DESC
+          'searchValue' => filter_var($_GET['search']['value'], FILTER_SANITIZE_STRING), // Valor Buscado
+          'usuario_id' => $_SESSION['usuario_id'],
+       ];
+       /** Número total de registros sin filtrar */
+       $totalRegistros = $this->inscripcionFinalModel->countInscripcionMesasUserDt(null, $_SESSION['usuario_id']);
+       /**Número total de registros con filtro */
+       $totalRegistrosFiltrado = $this->inscripcionFinalModel->countInscripcionMesasUserDt($data['searchValue'], $_SESSION['usuario_id']);
+       $mesasFinales = $this->inscripcionFinalModel->getInscripcionMesasUserDt($data);
+       /**Armamos el dataResponse */
+       foreach ($mesasFinales as $key => $mesaFinal) {
+          $dataResponse[] = [
+             'key' => ($key + 1),
+             'materia' => $mesaFinal->materia,
+             'aula' => $mesaFinal->aula,
+             'fecha' => $mesaFinal->fecha,
+             'created_at' => $mesaFinal->created_at,
+          ];
+       }
+       /**Armamos el response para el Ajax */
+       $response = [
+          "draw" => intval($draw),
+          "iTotalRecords" => $totalRegistros,
+          "iTotalDisplayRecords" => $totalRegistrosFiltrado,
+          "aaData" => $dataResponse
+       ];
+       /**Mostramos el resultado en JSON */
+       echo json_encode($response);
+    }
    /**
     * Método por el cual se registra o asignas 
     * materias al usuario y son registradas
